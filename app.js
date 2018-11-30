3535f2c6f18c88301159707cf634aac9d1a4c517
gateno <gerardo.ateno@gnp.com.mx>
2018-11-30 09:26:31 -0600

    Prueba exitosa del api rest

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ var express = require("express"),
     methodOverride = require("method-override"),
     mongoose = require("mongoose");
 
+//Connect to DB
+mongoose.connect('mongodb://127.0.0.1:27017/tvshows',{useNewUrlParser: true});
+
+
+//Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+//Import Model and Controller
+var models = require('./models/tvshow');
+var TVShowCtrl = require('./controllers/tvshows');
+
+//Example router
 var router = express.Router();
 
 router.get('/', function(req,res){
@@ -16,7 +26,18 @@ router.get('/', function(req,res){
 
 app.use(router);
 
-mongoose.connect('mongodb://127.0.0.1:27017/tvshows',{useNewUrlParser: true});
+//API routes
+var tvshows = express.Router();tvshows.route('/tvshows')
+    .get(TVShowCtrl.findAllTVShows)
+    .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+    .get(TVShowCtrl.findById)
+    .put(TVShowCtrl.updateTVShow)
+    .delete(TVShowCtrl.deleteTVShow);
+
+app.use(tvshows);
+
 
 app.listen(3000,function(){
     console.log("Node server is running on http://localhost:3000/");
